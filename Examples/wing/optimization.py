d3130a2a7706a6c3ea1f5e158d12087b25e881a0
Engelstad <sengelstad6@ae-smdo-405027.ae.gatech.edu>
2021-12-20 16:36:46 -0500

    Wing example improved

--- Examples/wing/optimization.py
+++ Examples/wing/optimization.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+from caps2tacs import Caps2Tacs
+#can't import setup Function from somewhere else, causes capsLock issue
+#from setupFunction import setupCAPS
+from paropt import ParOpt
+from mpi4py import MPI
+
 def setupCAPS(egadsAim,tacsAim):
+    #setup function for panel.csm
     
 	#Egads Aim section, for mesh
     egadsAim.input.Edge_Point_Min = 15
-    egadsAim.input.Edge_Point_Max = 20
+    egadsAim.input.Edge_Point_Max = 40
     
     egadsAim.input.Mesh_Elements = "Quad"
     
@@ -18,39 +26,129 @@ def setupCAPS(egadsAim,tacsAim):
     # Set analysis type
     tacsAim.input.Analysis_Type = "Static"
     
-    #materials section
+    #materials section    
     madeupium    = {"materialType" : "isotropic",
                     "youngModulus" : 72.0E9 ,
                     "poissonRatio": 0.33,
                     "density" : 2.8E3,
                     "tensionAllow" :  20.0e7}
     
-    tacsAim.input.Material = {"Madeupium": madeupium}
+    tacsAim.input.Material = {"madeupium": madeupium}
     
     # Material properties section
-    shell  = {"propertyType" : "Shell",
-              "membraneThickness" : 0.006,
+    ribshell  = {"propertyType" : "Shell",
+              "membraneThickness" : 0.02,
               "material"        : "madeupium",
               "bendingInertiaRatio" : 1.0, # Default
               "shearMembraneRatio"  : 5.0/6.0} # Default
     
-    tacsAim.input.Property = {"plate": shell}
+    sparshell = {"propertyType" : "Shell",
+              "membraneThickness" : 0.05,
+              "material"        : "madeupium",
+              "bendingInertiaRatio" : 1.0, # Default
+              "shearMembraneRatio"  : 5.0/6.0} # Default
+    
+    tacsAim.input.Property = {"wing": ribshell}
     
     # constraint section
-    constraint = {"groupName" : "edge",
+    constraints = {"groupName" : "wingRoot",
                   "dofConstraint" : 123456}
     
-    tacsAim.input.Constraint = {"edgeConstraint": constraint}
-    
-    #loads section
-    pressload = 1.0e5
+    tacsAim.input.Constraint = {"fixRoot": constraints}
     
     # Set load
-    load = {"groupName" : "plate",
-            "loadType" : "Pressure",
-            "pressureForce" : pressload}
+    liftload = {"groupName"         : "bottomWing",
+        "loadType"          : "GridForce",
+        "forceScaleFactor"  : 1.0e5,
+        "directionVector"   : [0.0, 1.0, 0.0]}
     
     # Set loads
-    tacsAim.input.Load = {"appliedPressure": load }
+    tacsAim.input.Load = {"lift": liftload }
+
+#ParOpt Optimization Class
+class Optimization(ParOpt.Problem):
+    def __init__(self):
+        self.problem = Caps2Tacs("afrl_wing4.csm", setupCAPS)
+        
+        self.nvar = 6 #number of design var
+        ncon = 2
+        nblock = 1
+        super(Optimization, self).__init__(MPI.COMM_SELF, self.nvar, ncon, nblock)
+        self.setBounds()
+    def setBounds(self,maxStress=1.0,minStress=0.0):
+        self.maxStress = maxStress
+        self.minStress = minStress
+    def getVarsAndBounds(self, x, lb, ub):
+        """Get the variable values and bounds"""
+        initialValues = [40,6.0,0.5,5.0,30.0,5.0]
+        upperBounds = [100,10.0,10.0,10.0,50.0,20.0]
+        lb[:] = 1e-3
+        for i in range(self.nvar):
+            ub[i] = upperBounds[i]
+            x[i] = initialValues[i]
+        return
+    def evalObjCon(self, x):
+        """
+        Return the objective, constraint and fail flag
+        """
+        #run the solver
+        self.problem.fullSolve(x[:])
+
+        fail = 0
+        obj = self.problem.mass() #mass
+
+        maxConstr = self.maxStress - self.problem.vm_stress()
+        minConstr = self.problem.vm_stress() - self.minStress
+        con = [maxConstr,minConstr] #vmstress
+
+        return fail, obj, con
+
+    def evalObjConGradient(self, x, g, A):
+        """
+        Return the objective, constraint and fail flag
+        """
+        #run the solver
+        self.problem.fullSolve(x[:])
+        
+        fail = 0
+        g[:] = self.problem.mass_grad()
+        
+        stress_grad = self.problem.vm_stress_grad()
+        A[0][:] = -stress_grad
+        A[1][:] = stress_grad
+        
+        return fail
+    def printObjCon(self, x):
+        fail, obj, con = self.evalObjCon(x)
+        print("\n")
+        print("Objective Value: ",obj)
+        print("Constraint Value(s): ",con)
+
+## Optimization problem defined here ##
+
+#run options: check, run
+option = "check"
     
-    return 0
+myOpt = Optimization()
+
+
+if (option == "check"):
+    #myOpt.problem.checkGradients()
+    x = [1.75002251e+01, 8.24977495e+00, 1.00606643e-03, 2.75022526e+00, 2.87354287e+01, 9.49976381e+00]
+    myOpt.problem.viewGeometry(x)
+elif (option == "run"):
+    myOpt.setBounds(maxStress=2.0,minStress=0.5)
+    
+    options = {
+        'algorithm': 'mma'}
+    
+    # Set up the optimizer
+    opt = ParOpt.Optimizer(myOpt, options)
+    
+    #Set a new starting point
+    opt.optimize()
+    x, z, zw, zl, zu = opt.getOptimizedPoint()
+    
+    #print optimized information
+    myOpt.printObjCon(x)
+    myOpt.problem.printDesignVariables(x[:])
